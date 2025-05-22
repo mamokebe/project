f9755fffbcf63ad80b2ee5da724f75eb11b1f09f
mamokebe <kebedebekele7@gmail>
2025-05-21 22:11:45 -0500

    new update

--- frontend/src/pages/MyOrders.jsx
+++ frontend/src/pages/MyOrders.jsx
@@ -40,7 +40,7 @@ const MyOrders = () => {
             <div key={index} className={`relative bg-white text-gray-500/700 ${order.items.length!==index+1&&"border-b"} border-gray-300 flex flex-col md:flex-row md:items-center justify-between p-4 py-5 md:gap-16 w-full max-w-4xl`}>
               <div className='flex items-center mb-4 md:mb-0'>
                 <div className='bg-primary/10 p-4 rounded-lg'>
-                  <img className='w-16 h-16' src={item?.product?.image} alt={item.name} />
+                  <img className='w-16 h-16' src={item?.product?.image} alt={item?.name} />
                 </div>
                 <div className='ml-4'>
                   <h2 className='text-xl font-medium text-gray-800'>{item?.product?.name}</h2>
@@ -52,7 +52,7 @@ const MyOrders = () => {
                 <p className='text-gray-500 font-medium'>Status: {order.status}</p>
                 <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
               </div>
-              <p className='text-primary text-lg font-medium'>Amount: {currency}{item?.product?.offerPrice * item.quantity}</p>
+              <p className='text-primary text-lg font-medium'>Amount: {currency}{item?.product?.offerPrice * item?.quantity}</p>
             </div>
           ))}
           
